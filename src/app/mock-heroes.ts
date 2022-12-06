9842b4447e08e2e7eb0f84c584fb4cc9c8eaada3
JanardanPethani <jpethani11@gmail.com>
2022-12-06 12:32:05 +0530

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Goku' },
+  { id: 13, name: 'Gohan' },
+  { id: 14, name: 'Luffy' },
+  { id: 15, name: 'Satoru' },
+  { id: 16, name: 'Yuji' },
+];
